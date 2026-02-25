79fa93526f69fc0396948fd376b5f1cd1d427699
Rayanec1 <chakirrayan008@gmail.com>
2026-02-25 09:03:41 -0800

    Add Security entity

---
+++ src/main/java/com/example/demo/entities/src/main/java/com/example/demo/src/main/java/com/example/demo/entities/Security.java
@@ -0,0 +1,44 @@
+package com.example.demo.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long securityId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    public Security() {}
+
+    public Security(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public Long getSecurityId() {
+        return securityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
