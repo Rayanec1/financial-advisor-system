ca85db3175dc9f387e038b97a56f920c640c5721
Rayanec1 <chakirrayan008@gmail.com>
2026-02-25 09:02:44 -0800

    Add Portfolio entity

---
+++ src/main/java/com/example/demo/entities/src/main/java/com/example/demo/entities/Portfolio.java
@@ -0,0 +1,44 @@
+package com.example.demo.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "client_id", unique = true)
+    private Client client;
+
+    private String createdDate;
+
+    public Portfolio() {}
+
+    public Portfolio(Client client, String createdDate) {
+        this.client = client;
+        this.createdDate = createdDate;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+}
